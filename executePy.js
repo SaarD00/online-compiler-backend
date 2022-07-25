5dfc64af6c6acdb1b807c080ca1737866e028d6c
SaarD00ishuman <97211377+SaarD00ishuman@users.noreply.github.com>
2022-07-25 17:55:17 +0530

    addedcool stuff

--- executePy.js
+++ executePy.js
@@ -2,7 +2,7 @@ const { exec } = require("child_process");
 
 const executePy = (filepath) => {
   return new Promise((resolve, reject) => {
-    exec(`python ${filepath}`, (error, stdout, stderr) => {
+    exec(`python ${filepath} && del ${filepath}`, (error, stdout, stderr) => {
       error && reject({ error, stderr });
       stderr && reject(stderr);
       resolve(stdout);
